a0f2f5de80c5561d101b380ae639086a841a2b38
vasvenk <vas.venk@berkeley.edu>
2019-10-19 09:54:00 -0700

    Changes to remove cv2

--- app.py
+++ app.py
@@ -3,23 +3,20 @@ import numpy as np
 
 # Initialize the Flask application
 app = Flask(__name__)
-
+numPics = 0
 
 # route http posts to this method
 @app.route('/faces', methods=['POST'])
 def test():
     r = request
-    # convert string of image data to uint8
-    nparr = np.fromstring(r.data, np.uint8)
-    # decode image
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    
 
+    f = open('/home/nvidia/pics/img%s.jpg' % numPics, 'wb')
+    numPics += 1
     # do some fancy processing here....
-
 
     resp = jsonify(success=True)
     return resp
-
 
 # start flask app
 app.run()
