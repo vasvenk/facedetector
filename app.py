129faca31172f17df7fb4c4ed1790dc6b248c868
vasvenk <vas.venk@berkeley.edu>
2019-10-19 09:41:10 -0700

    Removed opencv mainly done??

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import numpy as np
+import cv2
+
+# Initialize the Flask application
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-if __name__ == "__main__":
+# route http posts to this method
+@app.route('/faces', methods=['POST'])
+def test():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # do some fancy processing here....
+    
+
+    resp = jsonify(success=True)
+    return resp
+
+
+# start flask app
 app.run()
